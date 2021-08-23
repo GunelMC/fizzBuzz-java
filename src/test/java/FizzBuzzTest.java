e7178381da56a9c17e7229e31abe34f338b6db25
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-08-23 14:41:32 +0100

    Fizzbuzz generates correctly up to Fifteen

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -23,4 +23,11 @@ public class FizzBuzzTest {
         String result = subject.generate(5);
         assertEquals("1, 2, Fizz, 4, Buzz", result);
     }
+
+    @Test
+    void testGenerateListsNumbersUpToFifteen() {
+        FizzBuzz subject = new FizzBuzz();
+        String result = subject.generate(15);
+        assertEquals(  "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz", result);
+    }
 }
